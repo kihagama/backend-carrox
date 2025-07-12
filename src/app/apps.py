eced1c7e9aa1cc7461cc6074eb99382a890cf072
2023bcs002 <2023bcs002@std.must.ac.ug>
2025-07-12 20:01:28 +0300

    hhk

--- src/app/apps.py
+++ src/app/apps.py
@@ -2,4 +2,4 @@ from django.apps import AppConfig
 
 class AppConfigName(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
-    name = 'app'  # ✅ MUST match your import path!
+    name = 'src.app'  # ✅ MUST match your import path!
