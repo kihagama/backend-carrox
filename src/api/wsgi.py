98c970c3991a60aeaf3299421f507ea45d1a2ceb
2023bcs002 <2023bcs002@std.must.ac.ug>
2025-07-12 18:36:43 +0300

    more

--- src/api/wsgi.py
+++ src/api/wsgi.py
@@ -2,6 +2,6 @@ import os
 from django.core.wsgi import get_wsgi_application
 
 # ✅ IMPORTANT: use the full dotted path relative to your project root
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'api.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'src.api.settings')
 
 application = get_wsgi_application()
