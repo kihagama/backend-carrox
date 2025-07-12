c7ded756e8613049082a3c1ed0ee97f0d2e80a19
2023bcs002 <2023bcs002@std.must.ac.ug>
2025-07-12 19:53:54 +0300

    mino

--- src/api/settings.py
+++ src/api/settings.py
@@ -38,7 +38,7 @@ INSTALLED_APPS = [
     'rest_framework_simplejwt',
     'rest_framework',
     'django_rest_passwordreset',
-    'app.apps.AppConfigName',
+    'src.app',
    'cloudinary',
     'cloudinary_storage',
 ]
