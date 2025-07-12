18698a717fee37adcffda96cd89e74c1899d70cb
kihagama <166952048+kihagama@users.noreply.github.com>
2025-07-12 15:39:06 +0300

    Update wsgi.py

--- src/api/wsgi.py
+++ src/api/wsgi.py
@@ -1,17 +1,10 @@
-"""
-WSGI config for app project.
-
-It exposes the WSGI callable as a module-level variable named ``application``.
-
-For more information on this file, see
-https://docs.djangoproject.com/en/5.0/howto/deployment/wsgi/
-"""
-
 import os
+import sys
+
+sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
 
 from django.core.wsgi import get_wsgi_application
 
-# ✅ Corrected to match your actual folder structure
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'api.settings')
 
 application = get_wsgi_application()
