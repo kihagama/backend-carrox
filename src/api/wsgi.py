ad4625fed717c0a9313120ee7d1d4e922b5e9c1a
2023bcs002 <2023bcs002@std.must.ac.ug>
2025-07-13 08:09:10 +0300

    come

--- src/api/wsgi.py
+++ src/api/wsgi.py
@@ -3,5 +3,5 @@ import sys
 sys.path.append('/opt/render/project/src')  # Add src/ to Python path
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'src.api.settings')  # Use src.api.settings
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'api.settings')  # Use src.api.settings
 application = get_wsgi_application()
--- src/app/apps.py
+++ src/app/apps.py
@@ -2,4 +2,4 @@ from django.apps import AppConfig
 
 class AppConfigName(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
-    name = 'src.app'  # ✅ MUST match your import path!
+    name = 'app'  # ✅ MUST match your import path!
